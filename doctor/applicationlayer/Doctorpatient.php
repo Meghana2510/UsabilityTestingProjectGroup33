458549c4bb2ddb733b04cfb7ca5bfc6fab274091
Meghana Thimmapuram <89931583+Meghana2510@users.noreply.github.com>
2022-04-20 21:17:34 +0530

    Add files via upload

---
+++ doctor/applicationlayer/Doctorpatient.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Doctor Patient</title>
+	<link rel="stylesheet"  href="doctorpatient.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="id=edge">
+</head>
+<center><header>
+	<h1>Doctor<span>Patient</span></h1>
+
+</header></center>
+<body>
+	<div class="wrapper">
+		<div class="btn" > <a href="login3.php">
+
+<button type="button" class="buttonA" > <b>Admin</b></button>
+
+</div>
+
+</div>
+
+
+	<div class="wrapper" style="text-decoration: none;">
+	<div class="btn" > <a href="login.php"  >
+<button type="button" class="buttonP"><b>Patient</b></button>
+</div>
+
+</div>
+
+
+	<div class="wrapper" style="text-decoration: none;">
+	<div class="btn" > <a href="login2.php"  >
+<button type="button" class="buttonD"><b>Doctor</b></button>
+        </a>
+</div>
+
+</div>
+
+	
+
+
+ 
+	
+
+
+
+
+</body>
+</html>
